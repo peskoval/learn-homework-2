ce11542164aa843c41fdcb91ab0026df02409ee3
Aleksandra Peskova <peskoval88@gmail.com>
2026-06-09 15:11:03 +0300

    datetime

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -7,13 +7,15 @@
 2. Превратите строку "01/01/20 12:10:03.234567" в объект datetime
 
 """
+from datetime import datetime, timedelta
 
 def print_days():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    now = datetime.today()
+    return now - timedelta(days=1), now, now - timedelta(days=30)
 
 
 def str_2_datetime(date_string):
@@ -21,7 +23,7 @@ def str_2_datetime(date_string):
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    return datetime.strptime(date_string, '%d/%m/%y %H:%M:%S.%f')
 
 if __name__ == "__main__":
     print_days()
