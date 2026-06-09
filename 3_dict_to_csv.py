9ee8ace5393047e5936c9f57b87c68c053fec35e
Aleksandra Peskova <peskoval88@gmail.com>
2026-06-09 15:52:05 +0300

    csv

--- 3_dict_to_csv.py
+++ 3_dict_to_csv.py
@@ -9,13 +9,27 @@
 2. Запишите содержимое списка словарей в файл в формате csv
 
 """
+from csv import DictWriter, QUOTE_NONNUMERIC
+
+
+PERSONALITIES = [
+    {'name': 'Albert', 'age': 34, 'job': 'Scientist'},
+    {'name': 'Charles', 'age': 47, 'job': 'Pediatrician'},
+    {'name': 'Genry', 'age': 23, 'job': 'Mechanic'},
+    {'name': 'Martha', 'age': 56, 'job': 'Dog expert'},
+    {'name': 'Mary', 'age': 29, 'job': 'Accountant'},
+]
+COLUMNS = ['name', 'age', 'job']
 
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    with open('personalities.csv', 'w', encoding='utf-8', newline='') as file:
+        writer = DictWriter(file, fieldnames=COLUMNS, delimiter=';', quoting=QUOTE_NONNUMERIC)
+        writer.writeheader()
+        writer.writerows(PERSONALITIES)
 
 if __name__ == "__main__":
     main()
